863b20f000022405f89ccafe51fa574f728cbc41
Nikita Kanunov <Nkanunov@gmail.com>
2018-12-01 21:47:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/testactivity/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.android.testactivity
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun summation(view: View) {
+        val cifra_1 = operand_1.text.toString()
+        val cifra_2 = operand_2.text.toString()
+
+        if ((cifra_1 == "" && cifra_2 == "") || (cifra_1 == "" || cifra_2 == ""))
+            Snackbar.make(root_layout, "Данные не введены ", Snackbar.LENGTH_SHORT).show()
+
+
+            else {
+                val sum: Double = cifra_1.toDouble() + cifra_2.toDouble()
+
+                result.text = sum.toString()
+            }
+    }
+
+    fun minus(view: View) {
+        val cifra_1 = operand_1.text.toString()
+        val cifra_2 = operand_2.text.toString()
+
+        if ((cifra_1 == "" && cifra_2 == "") || (cifra_1 == "" || cifra_2 == ""))
+            Snackbar.make(root_layout, "Данные не введены ", Snackbar.LENGTH_SHORT).show()
+
+
+        else {
+            val sum: Double = cifra_1.toDouble() - cifra_2.toDouble()
+
+            result.text = sum.toString()
+        }
+    }
+
+    fun multiply(view: View) {
+        val cifra_1 = operand_1.text.toString()
+        val cifra_2 = operand_2.text.toString()
+
+        if ((cifra_1 == "" && cifra_2 == "") || (cifra_1 == "" || cifra_2 == ""))
+            Snackbar.make(root_layout, "Данные не введены ", Snackbar.LENGTH_SHORT).show()
+
+
+        else {
+            val sum: Double = cifra_1.toDouble() * cifra_2.toDouble()
+
+            result.text = sum.toString()
+        }
+    }
+
+    fun div(view: View) {
+        val cifra_1 = operand_1.text.toString()
+        val cifra_2 = operand_2.text.toString()
+
+        if ((cifra_1 == "" && cifra_2 == "") || (cifra_1 == "" || cifra_2 == ""))
+            Snackbar.make(root_layout, "Данные не введены ", Snackbar.LENGTH_SHORT).show()
+        else
+            if (cifra_2.toDouble() == 0.0) {
+                Snackbar.make(
+                    root_layout,
+                    "Второе число не может равняться 0, если вы хотите делить",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            } else {
+                val sum: Double = cifra_1.toDouble() / cifra_2.toDouble()
+                result.text = sum.toString()
+            }
+    }
+
+
+}
+
---
+++ gradlew.bat
@@ -0,0 +1,46 @@
+<application>
+  <component name="ActionsCollector">
+    <counts>
+      <entry action="$Copy" count="1" />
+      <entry action="$Delete" count="4" />
+      <entry action="$Paste" count="2" />
+      <entry action="$Redo" count="11" />
+      <entry action="$SelectAll" count="32" />
+      <entry action="$Undo" count="548" />
+      <entry action="Android.BuildApk" count="1" />
+      <entry action="Android.RunAndroidAvdManager" count="2" />
+      <entry action="Android.RunAndroidSdkManager" count="3" />
+      <entry action="Android.RunLayoutInspector" count="1" />
+      <entry action="Android.SyncProject" count="3" />
+      <entry action="ChooseRunConfiguration" count="1" />
+      <entry action="CleanGradleProject" count="3" />
+      <entry action="CodeCompletion" count="78" />
+      <entry action="ConvertJavaToKotlin" count="3" />
+      <entry action="DialogOkAction Enter" count="5" />
+      <entry action="EditorBackSpace" count="1241" />
+      <entry action="EditorChooseLookupItem" count="151" />
+      <entry action="EditorChooseLookupItemReplace" count="1" />
+      <entry action="EditorContextInfo" count="1" />
+      <entry action="EditorCopy" count="138" />
+      <entry action="EditorCut" count="52" />
+      <entry action="EditorDelete" count="257" />
+      <entry action="EditorDeleteLine" count="26" />
+      <entry action="EditorDown" count="529" />
+      <entry action="EditorEnter" count="246" />
+      <entry action="EditorIndentSelection" count="7" />
+      <entry action="EditorLeft" count="714" />
+      <entry action="EditorPaste" count="242" />
+      <entry action="EditorRight" count="635" />
+      <entry action="EditorTab" count="58" />
+      <entry action="EditorUp" count="191" />
+      <entry action="FileChooser.NewFolder" count="1" />
+      <entry action="FindNext" count="1" />
+      <entry action="Genymotion.gdm" count="2" />
+      <entry action="GotoAction" count="2" />
+      <entry action="GotoDeclaration" count="1" />
+      <entry action="HideAllWindows" count="9" />
+      <entry action="InvalidateCaches" count="2" />
+      <entry action="JumpToLastWindow" count="1" />
+      <entry action="Kotlin.NewFile" count="1" />
+      <entry action="MakeGradleProject" count="1" />
+      <entry action="NewClass" count="1"
